2b97222e75dbe283b72c48d8215e8137714b4942
Claire <clairefraser121@gmail.com>
2024-03-31 20:40:21 +0100

    Added TBT Education section

--- script/projectScript.js
+++ script/projectScript.js
@@ -1,29 +1,41 @@
-// projectScript.js
+// Function to open the project modal for BeWyld
+function openBeWyldModal() {
+  document.getElementById('bewyld-modal').style.display = 'block';
+}
 
-// Function to open the project modal
-function openProjectModal() {
-    document.getElementById('projectModal').style.display = 'block';
+// Function to close the project modal for BeWyld
+function closeBeWyldModal() {
+  document.getElementById('bewyld-modal').style.display = 'none';
+}
+
+// Function to open the project modal for The TBT Education App
+function openTBTEducationAppModal() {
+  document.getElementById('tbt-education-app-modal').style.display = 'block';
+}
+
+// Function to close the project modal for The TBT Education App
+function closeTBTEducationAppModal() {
+  document.getElementById('tbt-education-app-modal').style.display = 'none';
+}
+
+// Close the modal if the user clicks outside the modal content or presses the Esc key
+window.onclick = function (event) {
+  var modalBeWyld = document.getElementById('bewyld-modal');
+  var modalTBTEducationApp = document.getElementById('tbt-education-app-modal');
+  if (event.target == modalBeWyld) {
+      modalBeWyld.style.display = 'none';
   }
-  
-  // Function to close the project modal
-  function closeProjectModal() {
-    document.getElementById('projectModal').style.display = 'none';
+  if (event.target == modalTBTEducationApp) {
+      modalTBTEducationApp.style.display = 'none';
   }
-  
-  // Close the modal if the user clicks outside the modal content or presses the Esc key
-  window.onclick = function (event) {
-    var modal = document.getElementById('projectModal');
-    if (event.target == modal) {
-      modal.style.display = 'none';
-    }
-  };
-  
-  // Close the modal if the user presses the Esc key
-  window.onkeydown = function (event) {
-    var modal = document.getElementById('projectModal');
-    if (event.key === 'Escape') {
-      modal.style.display = 'none';
-    }
-  };
-  
-  
+};
+
+// Close the modal if the user presses the Esc key
+window.onkeydown = function (event) {
+  var modalBeWyld = document.getElementById('bewyld-modal');
+  var modalTBTEducationApp = document.getElementById('tbt-education-app-modal');
+  if (event.key === 'Escape') {
+      modalBeWyld.style.display = 'none';
+      modalTBTEducationApp.style.display = 'none';
+  }
+};
